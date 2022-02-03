b6233f7118ce182cc65f2b2d2278a8c94a0d9cf8
501 <assimilator531@gmail.com>
2022-02-03 17:59:31 +0900

    0203 stat learn python

--- 1.py
+++ 1.py
@@ -1,5 +1,8 @@
-from msilib.schema import LockPermissions
-from random import lognormvariate
+print('input your number : ', end = '')
+input_num1 = int(input())
 
+print('input your second number : ', end = '')
+input_num2 = int(input())
 
-print("hi")
+sum = input_num1+input_num2
+print('sum is %s' %sum)
---
+++ type.py
@@ -0,0 +1,10 @@
+#dynamically typed
+
+a = 1
+print(a)
+a = 1.1
+print(a)
+a = 's'
+print(a)
+a = 'sdfk'
+print(a)
