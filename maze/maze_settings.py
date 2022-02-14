4dcd7ecf73a2d547084c07f6375b76a7a5bb85c4
501 <assimilator531@gmail.com>
2022-02-14 12:16:22 +0900

    testing gitignore with creating venv

--- maze/maze_settings.py
+++ maze/maze_settings.py
@@ -2,61 +2,53 @@ class Const:
     def __init__(self) -> None:
         pass
     class File:
-        def __init__(self) -> None:
-            self.LOCATION_MAP = 'maze_map.txt'
+        LOCATION_MAP = 'maze_map.txt'
 
     class Tile:
-        def __init__(self) -> None:
-            self.WALL = 'X'
-            self.START = 'S'
-            self.FINISH = 'F'
-            self.VALID = 'o'
-            #directions
-            self.UP = 0
-            self.RIGHT = 1
-            self.DOWN = 2
-            self.LEFT = 3
+        WALL = 'X'
+        START = 'S'
+        FINISH = 'F'
+        VALID = 'o'
+        #directions
+        UP = 0
+        RIGHT = 1
+        DOWN = 2
+        LEFT = 3
 
     class Padding:
-        def __init__(self) -> None:
-            self.MAP = 1
-            self.WALL = 2
-            self.MAP_DISPLAY = ' '
+        MAP = 1
+        WALL = 2
+        MAP_DISPLAY = ' '
 
     class Input:
-        def __init__(self) -> None:
-            self.UP = '0'
-            self.RIGHT = '1'
-            self.DOWN = '2'
-            self.LEFT = '3'
+        UP = '0'
+        RIGHT = '1'
+        DOWN = '2'
+        LEFT = '3'
 CONST_FILE = Const.File()
 CONST_TILE = Const.Tile()
 CONST_PADDING = Const.Padding()
 CONST_INPUT = Const.Input()
 
 class Msg_text:
-    def __init__(self) -> None:
-        #direction msg
-        self.UP = ' up = 0 '
-        self.RIGHT = ' right = 1 '
-        self.DOWN = ' down = 2 '
-        self.LEFT = ' left = 3 '
-        self.INVALID = 'Invalid input!!'
-        self.ASK_DIRECTION = 'Where to go? : '
-        self.INFO = 'Moves [%d]   Goal %s   Current Location %s'
-        self.WELCOME = 'PRESS ENTER TO PLAY\n'
-        self.EMPTY = ''
+    UP = ' up = 0 '
+    RIGHT = ' right = 1 '
+    DOWN = ' down = 2 '
+    LEFT = ' left = 3 '
+    INVALID = 'Invalid input!!'
+    ASK_DIRECTION = 'Where to go? : '
+    INFO = 'Moves [%d]   Goal %s   Current Location %s'
+    WELCOME = 'PRESS ENTER TO PLAY\n'
+    EMPTY = ''
 MSG_TEXT = Msg_text()
 
 class Coord:
-    def __init__(self) -> None:
-        self.X = 0
-        self.Y = 1
+    X = 0
+    Y = 1
 COORD = Coord()
 
 class Debug:
-    def __init__(self) -> None:
-        self.MSG = '<<Debug Mode>>'
-        self.OPTION = '-d'
-        self.KEY = 'black sheep wall'
+    MSG = '<<Debug Mode>>'
+    OPTION = '-d'
+    KEY = 'black sheep wall'
 DEBUG = Debug()
